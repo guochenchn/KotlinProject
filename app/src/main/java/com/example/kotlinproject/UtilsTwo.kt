2f1af9d5c21a0e15095bc54276bf019f973c645e
zzh9376 <zzh9376@163.com>
2020-08-25 00:21:00 +0800

    kotlin的三种静态表示方式

---
+++ app/src/main/java/com/example/kotlinproject/UtilsTwo.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinproject
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
+import android.widget.Toast
+
+object UtilsTwo {
+    fun showToast(context : Context,message : String){
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show()
+    }
+
+private val displayMetrics = Resources.getSystem().displayMetrics
+    fun dp2px(dp : Float) : Float{
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp, displayMetrics)
+    }
+}
