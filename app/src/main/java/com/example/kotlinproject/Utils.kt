4cf227a0615c22634640bf98c1063c26db239add
guochen <guopchenchn@163.com>
2020-09-01 17:24:08 +0800

    自定义控件

--- app/src/main/java/com/example/kotlinproject/Utils.kt
+++ app/src/main/java/com/example/kotlinproject/Utils.kt
@@ -1,8 +1,12 @@
 package com.example.kotlinproject
 
 import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
 import android.widget.Toast
 
 fun showToast(context:Context,message:String){
     Toast.makeText(context,message,Toast.LENGTH_LONG).show()
 }
+
+val Float.px get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
--- app/src/main/java/com/example/kotlinproject/UtilsTwo.kt
+++ app/src/main/java/com/example/kotlinproject/UtilsTwo.kt
@@ -14,4 +14,6 @@ private val displayMetrics = Resources.getSystem().displayMetrics
     fun dp2px(dp : Float) : Float{
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp, displayMetrics)
     }
+
+
 }
